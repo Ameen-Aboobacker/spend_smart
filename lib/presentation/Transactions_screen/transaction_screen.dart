1513169bb91547c2e3cec4af3c6dca8ecaf60624
Ameen Aboobacker <ameenkaboobacker95@gmail.com>
2024-10-07 16:45:09 +0530

    transactions screen design

--- lib/presentation/Transactions_screen/transaction_screen.dart
+++ lib/presentation/Transactions_screen/transaction_screen.dart
@@ -1,17 +1,20 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
 class TransactionScreen extends StatelessWidget {
   const TransactionScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    Map<String, List<Map<String, dynamic>>> groupedTransactions = {
-      '1/10/2024': [
+    Map<DateTime, List<Map<String, dynamic>>> groupedTransactions = {
+      DateTime(2024, 10, 1): [
         {'category': 'food', 'type': 'expense', 'amount': 1000},
         {'category': 'health', 'type': 'expense', 'amount': '1000'},
         {'category': 'salary', 'type': 'income', 'amount': 1000}
       ],
-      '2/10/2024': [
+      DateTime(2024, 10, 2): [
         {'category': 'food', 'type': 'expense', 'amount': 1000},
         {'category': 'health', 'type': 'expense', 'amount': 1000},
         {'category': 'salary', 'type': 'income', 'amount': 1000},
@@ -19,13 +22,14 @@ class TransactionScreen extends StatelessWidget {
         {'category': 'health', 'type': 'expense', 'amount': 1000},
         {'category': 'salary', 'type': 'income', 'amount': 1000}
       ],
-      '3/10/2024': [
+      DateTime(2024, 10, 3): [
         {'category': 'food', 'type': 'expense', 'amount': 1000},
         {'category': 'health', 'type': 'expense', 'amount': 1000},
         {'category': 'salary', 'type': 'income', 'amount': 1000}
       ]
     };
-    List<String> dates = groupedTransactions.keys.toList();
+    List dates = groupedTransactions.keys.toList();
+
     TextStyle style =
         const TextStyle(fontSize: 16, fontWeight: FontWeight.w400);
     TextStyle style1 =
@@ -39,21 +43,57 @@ class TransactionScreen extends StatelessWidget {
               width: 25,
               child:
                   IconButton(onPressed: () {}, icon: const Icon(Icons.search))),
-          IconButton(
-            icon: const Icon(Icons.filter_list_rounded),
-            onPressed: () {},
+          PopupMenuButton(
+            icon: const Icon(
+              Icons.filter_list_rounded,
+              size: 30,
+            ),
+            itemBuilder: (context) => [
+              PopupMenuItem(
+                child: const Text('All'),
+                onTap: () => log('All'),
+              ),
+              const PopupMenuItem(child: Text('Income')),
+              const PopupMenuItem(child: Text('Expenses'))
+            ],
           )
         ],
         centerTitle: true,
         title: const Text('All Transactions'),
         bottom: PreferredSize(
-            preferredSize: const Size(double.infinity, 20),
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+            preferredSize: const Size(double.infinity, 40),
+            child: Container(
+              margin: const EdgeInsets.fromLTRB(5, 0, 20, 3),
               child: Row(
+                crossAxisAlignment: CrossAxisAlignment.center,
                 mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Text('LAST 30 DAYS', style: style),
+                  TextButton.icon(
+                    onPressed: () {
+                      showMenu(
+                          context: context,
+                          position: const RelativeRect.fromLTRB(0, 120, 0, 0),
+                          items: [
+                            const PopupMenuItem(child: Text('Today')),
+                            const PopupMenuItem(child: Text('This Week')),
+                            const PopupMenuItem(child: Text('This Month')),
+                            const PopupMenuItem(child: Text('Last 30 days')),
+                            const PopupMenuItem(child: Text('Last 6 months')),
+                            const PopupMenuItem(child: Text('Last year')),
+                            const PopupMenuItem(child: Text('Custom Year')),
+                            const PopupMenuItem(child: Text('Custom date')),
+                            const PopupMenuItem(child: Text('Custom Range')),
+                          ]);
+                    },
+                    label: Text(
+                      'LAST 30 DAYS',
+                      style: style.copyWith(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                      ),
+                    ),
+                    icon: const Icon(Icons.arrow_drop_down_outlined),
+                  ),
                   Text('2000', style: style),
                 ],
               ),
@@ -62,18 +102,17 @@ class TransactionScreen extends StatelessWidget {
       body: ListView.builder(
         itemCount: dates.length,
         itemBuilder: (context, index) {
-          String date = dates[index];
-
+          DateTime date = dates[index];
+          String newdate = DateFormat('dd-MMM-yy, EEEE').format(date);
           List<Map<String, dynamic>> transactions = groupedTransactions[date]!;
 
           return Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              // Date as a heading
               Padding(
                 padding: const EdgeInsets.all(8.0),
                 child: Text(
-                  date,
+                  newdate,
                   style: const TextStyle(
                     fontSize: 17,
                   ),
@@ -125,7 +164,9 @@ class TransactionScreen extends StatelessWidget {
                               children: [
                                 Text(transaction['amount'].toString(),
                                     style: style1),
-                                Text('Balance', style: style1),
+                                Text('Balance',
+                                    style:
+                                        style1.copyWith(color: Colors.green)),
                                 Text('29-sep', style: style1),
                               ],
                             ),
